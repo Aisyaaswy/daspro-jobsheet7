e469c5fcb4c16c5d62c2e7f8371430b2034d46da
aisyaaswy <aisyaidha@gmail.com>
2024-10-10 08:46:20 +0700

    jobsheet 7

--- SiakadFor03.java
+++ SiakadFor03.java
@@ -3,7 +3,9 @@ public class SiakadFor03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double nilai, tertinggi = 0, terendah = 100;
+        double nilai, tertinggi = 100, terendah = 0;
+        int lulus = 0, tidakLulus = 0;
+        int nilaiMinimal = 60;
 
         for (int i = 1; i <=10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -14,8 +16,15 @@ public class SiakadFor03 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= nilaiMinimal) {
+                lulus++; 
+            } else {
+                tidakLulus++;
+            }
         }
-        System.out.println("Nilai tertinggi: " + tertinggi);
-        System.out.println("NIlai terendah: " + terendah);
+        System.out.println("Nilai tertinggi : " + tertinggi);
+        System.out.println("NIlai terendah  : " + terendah);
+        System.out.println("Jumlah mahasiswa yang lulus    : " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus   : " + tidakLulus);
     }
 }
